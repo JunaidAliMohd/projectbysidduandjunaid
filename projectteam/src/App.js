bb5a8a47996ef6ceba6735947f6039672114ab22
MOHD JUNAID ALI <junaidali3089@gmail.com>
2023-05-11 23:16:56 +0530

    you can edit from here siddu

--- projectteam/src/App.js
+++ projectteam/src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <h1>Globel warming</h1>
       <p>Global warming is the long-term heating of Earth's surface observed since the pre-industrial period between 1850 and 1900 due to human activities, primarily fossil fuel burning, which increases heat-trapping greenhouse gas levels in Earth's atmosphere. This term is not interchangeable with the term "climate change."</p>
+      <p>siddu you can edit from here</p>
     </div>
   );
 }
