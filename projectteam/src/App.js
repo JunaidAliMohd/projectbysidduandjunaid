62074adad05ba8d728e37f7080f1c432941e3617
siddhanth kanukuntla <kanukuntlasiddhanth1@email.com>
2023-05-12 10:42:25 +0530

    added siddhanth

--- projectteam/src/App.js
+++ projectteam/src/App.js
@@ -7,6 +7,7 @@ function App() {
       <p>Global warming is the long-term heating of Earth's surface observed since the pre-industrial period between 1850 and 1900 due to human activities, primarily fossil fuel burning, which increases heat-trapping greenhouse gas levels in Earth's atmosphere. This term is not interchangeable with the term "climate change."</p>
       <p>siddu you can edit from here</p>
       <h3>siddhanth is edditing to the project junaid is created project</h3>
+      <h1>siddhanth</h1>
     </div>
   );
 }
